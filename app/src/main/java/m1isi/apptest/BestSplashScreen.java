2f33767b15a5555d268caa2990ef9a18e97e8bbc
godefroy29 <godefroy.29@gmail.com>
2016-03-07 20:49:31 +0100

    amélioration splashScreen

--- app/src/main/java/m1isi/apptest/BestSplashScreen.java
+++ app/src/main/java/m1isi/apptest/BestSplashScreen.java
@@ -1,6 +1,8 @@
 package m1isi.apptest;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +10,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class BestSplashScreen extends Activity {
 
+    Intent accueil;
+
+    Button btnAdmin;
+    Button btnConnexion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +26,41 @@ public class BestSplashScreen extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_best_splash_screen);
 
+        accueil = new Intent(this, Accueil.class);
+        btnAdmin = (Button) findViewById(R.id.btnAdmin);
+        btnConnexion = (Button) findViewById(R.id.btnConnexion);
+
+        btnAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(accueil);
+            }
+        });
+        btnConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (testConnexion()) {
+                    if (testIdentifiants()){
+                        startActivity(accueil);
+                    }else{
+                        Toast.makeText(BestSplashScreen.this, "Vérifiez vos identifiants.", Toast.LENGTH_SHORT);
+                    }
+                }else{
+                    Toast.makeText(BestSplashScreen.this, "Pas de connection à la base de données.", Toast.LENGTH_SHORT);
+                }
+
+            }
+        });
+    }
+
+    //TODO: verifier id
+    private boolean testIdentifiants() {
+        return true;
+    }
+
+    //TODO: verifier connexion
+    private boolean testConnexion() {
+        return true;
     }
 
 }
