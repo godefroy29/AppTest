b50f276d3fa98eac70b1ca7aa081605ca18f00cc
Menkusso <menkusso@gmail.com>
2016-03-23 14:07:27 +0100

    Demarre directement sur l'écran d'accueil

--- app/src/main/java/m1isi/apptest/MainActivity.java
+++ app/src/main/java/m1isi/apptest/MainActivity.java
@@ -1,20 +1,13 @@
 package m1isi.apptest;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressDialog progressDialog;
-Intent bestSplashScreen;
+    Intent bestSplashScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,44 +15,7 @@ Intent bestSplashScreen;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                progressDialog = ProgressDialog.show(MainActivity.this, "Check DB", "getNumberOfProject", true, false);
-                DatabaseTest dt = new DatabaseTest(MainActivity.this);
-                dt.getNumberOfProject();
-
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void getResult(String value){
-        System.out.println("\tFound value " + value);
-        progressDialog.dismiss();
         bestSplashScreen = new Intent(MainActivity.this, BestSplashScreen.class);
         startActivity(bestSplashScreen);
-        finish();
     }
 }
---
+++ app/src/main/java/m1isi/apptest/SplashScreen.java
@@ -0,0 +1,23 @@
+package m1isi.apptest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_best_splash_screen);
+
+    }
+
+}
