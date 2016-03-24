0e3fa44c349e9909b022597710b8cba142d6a82e
Menkusso <menkusso@gmail.com>
2016-03-24 17:22:12 +0100

    Finish

--- app/src/main/java/m1isi/apptest/TacheCreate.java
+++ app/src/main/java/m1isi/apptest/TacheCreate.java
@@ -1,13 +1,24 @@
 package m1isi.apptest;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TacheCreate extends AppCompatActivity {
+    ProgressDialog progressDialog;
+
+    Button btnValidate;
+    EditText editTextTitle;
+    EditText editTextDescription;
+    EditText editTextTime;
+    DatePicker datePickerStart;
+    DatePicker datePickerEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +27,30 @@ public class TacheCreate extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.projectCreate_create_of_ATask);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnValidate = (Button) findViewById(R.id.TaskCreateValidateButton);
+        editTextTitle = (EditText) findViewById(R.id.TaskCreateTitleEditText);
+        editTextDescription = (EditText) findViewById(R.id.TaskCreateDescriptionEditText);
+        editTextTime = (EditText) findViewById(R.id.TaskCreateTimeEditText);
+        datePickerStart = (DatePicker)findViewById(R.id.TaskCreateDateBeginDatePicker);
+        datePickerEnd = (DatePicker) findViewById(R.id.TaskCreateDateEndDatePicker);
+
+        btnValidate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                progressDialog = ProgressDialog.show(TacheCreate.this, getResources().getString(R.string.general_connecting), getResources().getString(R.string.TaskCreate_adding_task), false);
+                TacheCreateDB addTask = new TacheCreateDB(TacheCreate.this);
+                int id_identifiant = getIntent().getExtras().getInt("id_identifiant");
+                int id_projet = getIntent().getExtras().getInt("id_projet");
+                addTask.eAddTache(id_identifiant, id_projet, editTextTitle.getText().toString(), editTextDescription.getText().toString(),
+                        editTextTime.getText().toString(), new java.sql.Date(0), new java.sql.Date(0), 0);
             }
         });
     }
 
     public void eAddTacheDone(){
-
+        progressDialog.dismiss();
+        Toast.makeText(TacheCreate.this, R.string.TaskCreate_task_added, Toast.LENGTH_LONG).show();
+        finish();
     }
 
 }
--- app/src/main/java/m1isi/apptest/TacheCreateDB.java
+++ app/src/main/java/m1isi/apptest/TacheCreateDB.java
@@ -1,5 +1,7 @@
 package m1isi.apptest;
 
+import java.sql.Date;
+
 /**
  * Created by godef on 24/03/2016.
  */
@@ -15,15 +17,18 @@ public class TacheCreateDB  extends AbstractDatabaseClass {
         this.caller = caller;
     }
 
-    public void eAddTache(ItemTache myTache){
-        query = "INSERT INTO t_tache (tac_titre, tac_desc, tac_duree, tac_dateD, tac_F, id_statuttache) ";
+    public void eAddTache(int id_identifiant, int id_projet, String tac_titre, String tac_desc, String tac_duree, Date tac_dateD, Date tac_dateF,
+                          int id_statuttache){
+        query = "INSERT INTO t_tache (id_projet, id_identifiant, tac_titre, tac_desc, tac_duree, tac_dateD, tac_F, id_statuttache) ";
         query = query + " VALUES (";
-        query = query + "'" + myTache.tac_titre + "',";
-        query = query + "'" + myTache.tac_desc + "',";
-        query = query + myTache.tac_duree + ",";
-        query = query + myTache.tac_dateD + ",";
-        query = query + myTache.tac_dateF + ",";
-        query = query + myTache.id_statuttache + ")";
+        query = query + "'" + id_projet + "',";
+        query = query + "'" + id_identifiant + "',";
+        query = query + "'" + tac_titre + "',";
+        query = query + "'" + tac_desc + "',";
+        query = query + tac_duree + ",";
+        query = query + tac_dateD + ",";
+        query = query + tac_dateF + ",";
+        query = query + id_statuttache + ")";
         query = query + ";";
         isInsert = true;
         fc = functionCalled.eAddTache;
